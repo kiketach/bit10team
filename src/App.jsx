282007c3ae1d020a032fe547dd04a9c172474bc7
enrique abril <kiketachira@gmail.com>
2023-03-02 19:56:16 -0500

    API img Random

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Homepage } from "./Components/Homepage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div className="container text-center p-3 mb-2 bg-info text-light">
+        <h1>Coffe Dev's</h1>
+    
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="container">
+        <BrowserRouter>
+          <nav className="py-4 text-center">
+            <Link className="px-3" to="/homepage">
+              Interactua con Nosotros
+            </Link>
+          </nav>
+          <hr />
+          <Routes>
+            <Route path="/homepage" element={<Homepage />}></Route>
+          </Routes>
+        </BrowserRouter>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
